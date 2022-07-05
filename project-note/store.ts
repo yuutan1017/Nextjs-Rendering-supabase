2e3ec01c3632bf05158de4011726955e4ecb5ce6
yuutan1017 <n.you0611@gmail.com>
2022-07-05 18:30:08 +0900

    store

---
+++ project-note/store.ts
@@ -0,0 +1,2 @@
+import create from "zustand/react";
+import { EditedNote, EditedComment, State } from "./types/type";
--- project-note/types/type.ts
+++ project-note/types/type.ts
@@ -1,6 +1,35 @@
-import { ReactNode } from "react"
+import { ReactNode } from 'react';
 
-export interface Title {
-  title: string
-  children : ReactNode
-}
+export type Title = {
+  title: string;
+  children: ReactNode;
+};
+
+export type Comment = {
+  id: string;
+  content: string;
+  created_at: string;
+  note_id: string;
+  user_id: string | undefined;
+};
+
+export type Note = {
+  id: string;
+  title: string;
+  content: string;
+  created_at: string;
+  user_id: string;
+  comments: Comment[];
+};
+
+export type EditedComment = Omit<Comment, 'created_at' | 'user_id' | 'note_id'>;
+export type EditedNote = Omit<Note, 'created_at' | 'user_id' | 'comments'>;
+
+export type State = {
+  editedNote: EditedNote;
+  editedComment: EditedComment;
+  updateEditedNote: (payload: EditedNote) => void;
+  updateEditedComment: (payload: EditedComment) => void;
+  resetEditedNote: () => void;
+  resetEditedComment: () => void;
+};
